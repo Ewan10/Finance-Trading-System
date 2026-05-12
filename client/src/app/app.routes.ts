caf886b0fb8e8eac5e0f108ec17aa8ea8cf20af8
Ioannis <secgeorgiou@hotmail.com>
2026-05-12 21:04:08 +0300

    Development of the orders view

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,17 +1,3 @@
 import { Routes } from '@angular/router';
-import { MainLayout } from '../features/main-layout/main-layout';
-import { Orders } from '../features/orders/orders';
 
-export const routes: Routes = [
-     {
-    path: '',
-    component: MainLayout,
-    children: [
-      {
-        path: 'orders',
-        component: Orders
-      }
-    ]
-  }
-
-];
+export const routes: Routes = [];
