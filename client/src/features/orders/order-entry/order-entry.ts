ab72196ea7da15a3ae755fd93a2aae8ff3d4c21d
Ioannis <secgeorgiou@hotmail.com>
2026-05-13 13:25:32 +0300

    Restored missed files

---
+++ client/src/features/orders/order-entry/order-entry.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-entry',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-entry.html',
+  styleUrl: './order-entry.css',
+})
+export class OrderEntry {
+
+}
